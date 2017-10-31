1aeaa88af1e6960b246921689abc1f2d71be8736
Dayakar Reddy <dayakarreddy@dayakars-air.home>
2017-10-31 18:50:51 -0400

    initial commit

---
+++ OBJ-APP/OBJ-APP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OBJ-APP
+//
+//  Created by Dayakar Reddy on 10/31/17.
+//  Copyright © 2017 Dayakar Reddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OBJ-APP/OBJ-APP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OBJ-APP
+//
+//  Created by Dayakar Reddy on 10/31/17.
+//  Copyright © 2017 Dayakar Reddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
